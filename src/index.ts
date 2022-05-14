d862ecc65ac0cad17620f195bc02be6bfc099de8
Joel Escobar <alu0101130408@ull.edu.es>
2022-05-14 18:50:25 +0000

    primera sesion finalizada

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/*
+import express = require('express');
+import './database/mongoose';
+
+import {artistRouter} from './routers/artistRouter';
+import {songRouter} from './routers/songRouter';
+import {playlistRouter} from './router/playlistRouter';
+
+const app = express();
+app.use(express.json());
+app.use(artistRouter);
+app.use(songRouter);
+app.use(playlistRouter);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor desplegado en ${port}`);
+});
+*/
