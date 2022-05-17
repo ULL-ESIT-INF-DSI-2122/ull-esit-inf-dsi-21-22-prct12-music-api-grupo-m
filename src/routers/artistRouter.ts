5a12fb03e3779e3f93aa0824e033d823d388a4dc
Joel Escobar <alu0101130408@ull.edu.es>
2022-05-17 17:07:18 +0000

    metodos get de artista

--- src/routers/artistRouter.ts
+++ src/routers/artistRouter.ts
@@ -3,6 +3,33 @@ import {artistModel} from '../schema/artistSchema';
 
 export const artistRouter = express.Router();
 
+
+artistRouter.get('/artist', async (req, res) => {
+  const filter = req.query.name?{name: req.query.name.toString()}:{};
+  try {
+    const resultSearch = await artistModel.find(filter);
+    if (resultSearch.length !== 0) {
+      return res.send(resultSearch);
+    }
+    return res.status(404).send();
+  } catch (error) {
+    return res.status(500).send();
+  }
+});
+
+
+artistRouter.get('/artist/:id', async (req, res) => {
+  try {
+    const artistFindById = await artistModel.findById(req.params.id);
+    if (!artistFindById) {
+      return res.status(404).send();
+    }
+    return res.send(artistFindById);
+  } catch (error) {
+    return res.status(500).send();
+  }
+});
+
 artistRouter.post('/artist', async (req, res) => {
   const artista = new artistModel(req.body);
   try {
