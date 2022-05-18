3c0b4a0d7df54db2091df155fb3282e3c4b40c83
Joel Escobar <alu0101130408@ull.edu.es>
2022-05-18 16:26:00 +0000

    metodos del rrouter song get y delete

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import express = require('express');
 import './database/mongoose';
 
 import {artistRouter} from './routers/artistRouter';
-// import {songRouter} from './routers/songRouter';
+import {songRouter} from './routers/songRouter';
 // import {playlistRouter} from './router/playlistRouter';
 
 const app = express();
 app.use(express.json());
 app.use(artistRouter);
-// app.use(songRouter);
+app.use(songRouter);
 // app.use(playlistRouter);
 
 const port = process.env.PORT || 3000;
--- src/routers/songRouter.ts
+++ src/routers/songRouter.ts
@@ -16,12 +16,65 @@ songRouter.get('/song', async (req, res) => {
   }
 });
 
+
+songRouter.get('/song/:id', async (req, res) => {
+  try {
+    const songFindById = await songModel.findById(req.params.id);
+    if (!songFindById) {
+      return res.status(404).send();
+    }
+    return res.send(songFindById);
+  } catch (error) {
+    return res.status(500).send();
+  }
+});
+
+
+// No se que hacer con el calulo de los oyentes
 songRouter.post('/song', async (req, res) => {
+  console.log(req.body.author);
   const song = new songModel(req.body);
+  // Recorremos el artista o los artistas que se le ha pasado.
+  // Indexamos la cancion creada en el array de canciones del artista.
+  // realizamos el calculo de oyentes mensuales del artista.
   try {
     await song.save();
     res.status(201).send(song);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+// Eliminar artistas
+songRouter.delete('/song', async (req, res) => {
+  console.log(`Se va a eliminar la canción: ${req.query.name}`);
+  if (!req.query.name) {
+    return res.status(400).send({
+      error: 'Se debe introducir el nombre de una cancion',
+    });
+  }
+
+  try {
+    const songDeleted = await songModel.findOneAndDelete({name: req.query.name.toString()});
+    if (!songDeleted) {
+      return res.status(404).send();
+    }
+    return res.send(songDeleted);
+  } catch (error) {
+    return res.status(400).send();
+  }
+});
+
+songRouter.delete('/song/:id', async (req, res) => {
+  try {
+    const songDeleted = await songModel.findByIdAndDelete(req.params.id);
+
+    if (!songDeleted) {
+      return res.status(404).send();
+    }
+
+    return res.send(songDeleted);
+  } catch (error) {
+    return res.status(400).send();
   }
 });
